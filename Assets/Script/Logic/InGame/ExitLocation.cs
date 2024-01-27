4fffac1bd013389473c8b168c9c2c8c3288993da
rlatkddn212 <rlatkddn212@naver.com>
2024-01-27 18:02:45 +0900

    dd

--- Assets/Script/Logic/InGame/ExitLocation.cs
+++ Assets/Script/Logic/InGame/ExitLocation.cs
@@ -6,12 +6,22 @@ using UnityEngine.SceneManagement;
 
 public class ExitLocation : MonoBehaviour
 {
+
+    //public void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.V))
+    //    {
+    //        FaceMgr.In.StopDetect();
+    //        SceneManager.LoadScene("TitleScene");
+    //    }
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Exit"); 
-            FaceMgr.In.StopDetect();
+            // FaceMgr.In.StopDetect();
             SceneManager.LoadScene("TitleScene");
         }
     }
